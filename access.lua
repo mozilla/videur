dfc67c0e08f11cd167aab99358839a17b2765124
Tarek Ziade <tarek@ziade.org>
2014-06-12 13:58:47 +0200

    initial version

---
+++ access.lua
@@ -0,0 +1,21 @@
+local cjson = require "cjson"
+
+-- config
+-- we are allowing 10 hits per 10 seconds
+local max_hits = 10
+local throttle_time = 10
+
+-- how many hits we got on this IP ?
+local stats = ngx.shared.stats
+local remote_ip = ngx.var.remote_addr
+local hits = stats:get(remote_ip)
+
+if hits == nil then
+  stats:set(remote_ip, 1, throttle_time)
+else
+  hits = hits + 1
+  stats:set(remote_ip, hits, throttle_time)
+  if hits >= max_hits then
+    ngx.exit(ngx.HTTP_FORBIDDEN);
+  end
+end
