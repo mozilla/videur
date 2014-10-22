900840d46bc9960e34347059bf9628ff06ede83e
Tarek Ziade <tarek@ziade.org>
2014-10-22 13:30:51 +0200

    hook up rate_limit to the limits/rate rule

--- lib/rate_limit.lua
+++ lib/rate_limit.lua
@@ -1,23 +1,34 @@
--- load the config
-local max_hits = tonumber(ngx.var.max_hits) + 1
-local throttle_time = tonumber(ngx.var.throttle_time)
 
--- how many hits we got on this IP ?
-local stats = ngx.shared.stats
-local remote_ip = ngx.var.remote_addr
-local hits = stats:get(remote_ip)
+function check_rate(target, config)
+  if config then
+    -- load the config
+    local max_hits = config.hits
+    local throttle_time = config.seconds
+    local match = config.match
 
-if hits == nil then
-  stats:set(remote_ip, 1, throttle_time)
-else
-  hits = hits + 1
-  stats:set(remote_ip, hits, throttle_time)
-  if hits >= max_hits then
-    ngx.status = 404
-    ngx.header.content_type = 'text/plain; charset=us-ascii'
-    ngx.print("Rate limit exceeded.")
-    ngx.exit(ngx.HTTP_OK)
+
+    -- how many hits we got on this IP ?
+    --local stats = ngx.shared.stats
+    --local remote_ip = ngx.var.remote_addr
+    --local hits = stats:get(remote_ip)
+
+    --if hits == nil then
+    --    stats:set(remote_ip, 1, throttle_time)
+    --else
+    --    hits = hits + 1
+    --    stats:set(remote_ip, hits, throttle_time)
+    --    if hits >= max_hits then
+    --    ngx.status = 404
+    --    ngx.header.content_type = 'text/plain; charset=us-ascii'
+    --    ngx.print("Rate limit exceeded.")
+    --    ngx.exit(ngx.HTTP_OK)
+    --    end
+    --end
   end
 end
 
-return ngx.exit(ngx.OK)
+
+-- public interface
+return {
+  check_rate = check_rate
+}
--- lib/videur.lua
+++ lib/videur.lua
@@ -4,7 +4,7 @@
 local util = require "util"
 local spec_reader = require "spec_reader"
 local body = require "body_reader"
-
+local rate_limit = require "rate_limit"
 
 -- read the options from the Nginx config file
 local cached_spec = ngx.shared.cached_spec
@@ -21,5 +21,8 @@ end
 ngx.var.target, limits, params = spec_reader.match(spec_url, cached_spec)
 local max_size = util.size2int(limits.max_body_size) or util.size2int(ngx.var.max_body_size)
 
--- 3. control the bozy size
+-- 3. check the rating limit
+rate_limit.check_rate(ngx.var.target, limits.rates)
+
+-- 4. control the bozy size
 body.check_size(max_size)
